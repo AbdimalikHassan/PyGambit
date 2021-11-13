2c51719b082440563f8874e4798e83b9750c77e1
Abdimalik Hassan <amhassan@email.wm.edu>
2021-11-12 23:15:09 -0500

    Pawn Promotions & Enpassant (naive sol)

--- ChessMain.py
+++ ChessMain.py
@@ -49,12 +49,13 @@ def main():
                 if len(playerClicks) == 2: # after 2nd click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    if move in validMoves:
-                        gs.makeMove(move)
-                        moveMade = True
-                        sqSelected = () # reset user clicks
-                        playerClicks = []
-                    else:
+                    for i in range(len(validMoves)):
+                        if move == validMoves[i]:
+                            gs.makeMove(validMoves[i])
+                            moveMade = True
+                            sqSelected = () # reset user clicks
+                            playerClicks = []
+                    if not moveMade:
                         playerClicks = [sqSelected]
             # Key handler
             elif e.type == p.KEYDOWN:

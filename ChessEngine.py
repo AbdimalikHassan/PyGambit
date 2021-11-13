0bbad574d145e20c3d400ebcfe7905a92b6931c7
Abdimalik Hassan <amhassan@email.wm.edu>
2021-11-12 22:58:30 -0500

    Make moves (toddler)

--- ChessEngine.py
+++ ChessEngine.py
@@ -18,4 +18,33 @@ class GameState():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
         self.whiteToMove = True
-        self.moveLog = []        
+        self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move) # add move to move bank for undo
+        self.whiteToMove = not self.whiteToMove # toggle white turn
+
+class Move():
+    ranksToRows = {"1":7, "2":6, "3":5, "4":4, "5":3, "6":2, "7":1, "8":0}
+    rowsToRanks = {v:k for k,v in ranksToRows.items()}
+
+    filesToCols = {"a":0, "b":1, "c":2, "d":3,"e":4, "f":5, "g":6, "h":7}
+    colsToFiles = {v:k for k,v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        # Does not account for pawns only having destination noted
+        # Does not account for captured piece notation
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, row, col):
+        return self.colsToFiles[col] + self.rowsToRanks[row]
